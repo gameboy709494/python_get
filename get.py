7e32c23417dd09bb7e487a224253c26bf8e706ea
Weiqi Chen <weiqi_chen@outlook.com>
2015-11-08 14:26:34 +0800

    还在不断尝试中……

--- get.py
+++ get.py
@@ -20,10 +20,29 @@ sections = soup.find_all("section")
 
 print sections
 
+sections = soup.find_all(id="free")
+
+print sections
+
 
 last_a_tag = soup.find("section", id="free")
 print last_a_tag
 
+#class SSServer:
+#	def __init__(self, address, port, password):
+#		self.address = address
+#		self.port = port
+#		self.password = password
+#	def __init__(self):
+#		self.address = "null"
+#		self.port = 0
+#		self.password = "null"
+#
+#
+#class MyHTMLParser(HTMLParser):
+#	def handle_starttag(self, tag, attrs):
+#		if tag == 'section':
+#			for name,value in attrs:
 #class SSServer:
 #	def __init__(self, address, port, password):
 #		self.address = address
